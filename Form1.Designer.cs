7fbc445552fcf3eca97b4b5aab765542fbd04694
Ximunda <88gaster88@gmail.com>
2026-02-04 18:18:46 +0500

    Working circles

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,34 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.clear_btn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // clear_btn
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.clear_btn.Location = new System.Drawing.Point(12, 12);
+            this.clear_btn.Name = "clear_btn";
+            this.clear_btn.Size = new System.Drawing.Size(75, 23);
+            this.clear_btn.TabIndex = 0;
+            this.clear_btn.Text = "Clear";
+            this.clear_btn.UseVisualStyleBackColor = true;
+            this.clear_btn.Click += new System.EventHandler(this.clear_btn_Click);
             // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1112, 488);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clear_btn);
             this.Name = "mainForm";
             this.Text = "form";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.mainForm_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mainForm_MouseClick);
+            this.Resize += new System.EventHandler(this.mainForm_Resize);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button clear_btn;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -11,35 +11,93 @@ using System.Windows.Forms;
 
 namespace OOP_Laba_3
 {
-    
     public partial class mainForm : Form
     {
+        public Container container = new Container();
         public mainForm()
         {
             InitializeComponent();
+            
         }
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-    }
+        private void mainForm_MouseClick(object sender, MouseEventArgs e)
+        {
+            CCircle circle = new CCircle(e.X, e.Y);
+            container.add(circle);
+            Invalidate();
+        }
+
+        private void mainForm_Paint(object sender, PaintEventArgs e)
+        {
+            container.drawAll(e.Graphics);
+        }
+
+        private void mainForm_Resize(object sender, EventArgs e)
+        {
+            Invalidate();
+        }
+
+        private void clear_btn_Click(object sender, EventArgs e)
+        {
+            container.clear();
+            Invalidate();
+        }
+    };
     public class CCircle
     {
-        private int _radius;
-        private int _x;
-        private int _y;
-        public CCircle(int radius, int x, int y)
+        private int X { get; }
+        private int Y { get; }
+        private Color Circle_color { get; set; }
+        private int Radius;
+        public CCircle(int x, int y, int radius, Color color)
         {
-            _radius = radius;
-            _x = x;
-            _y = y;
+            Radius = radius;
+            X = x;
+            Y = y;
+            Circle_color = color;
+        }
+        public CCircle(int x, int y)
+        {
+            Radius = 20;
+            X = x;
+            Y = y;
+            Circle_color = Color.Black;
         }
         public void draw(Graphics g)
         {
-            using (Brush brush = new SolidBrush(Color.Black))
+            using (Pen pen = new Pen(Circle_color, 2))
             {
-                g.FillEllipse(brush, _x - _radius, _y - _radius, _radius * 2, _radius * 2);
+                g.DrawEllipse(pen, X - Radius, Y - Radius, Radius * 2, Radius * 2);
+            }
+        }
+    }
+    public class Container
+    {
+        private List<CCircle> Circles = new List<CCircle>();
+
+        public void add(CCircle circle)
+        {
+            Circles.Add(circle);
+        }
+        public void remove(CCircle circle)
+        {
+            Circles.Remove(circle);
+        }
+        public void clear()
+        {
+            
+            Circles.Clear();
+           
+        }
+        public void drawAll(Graphics g)
+        {
+            foreach (CCircle circle in Circles)
+            {
+                circle.draw(g);
             }
         }
     }
